3d1944f9a9816ae8ff6274bb00d8b521d4b670ff
Juanki019 <juanki.sgbh@gmail.com>
2023-04-19 13:21:04 +0200

    v1

---
+++ index.js
@@ -0,0 +1,50 @@
+$(function() {
+
+    // Scroll click on menu
+    $(".scroll").on("click", function() {   
+        $(this).each(function() {
+            const sectionTop = $(this.hash).offset().top;
+            $("html, body").animate({
+                scrollTop: sectionTop
+            }, 1500);
+        });
+    });
+    
+    
+    // Progress Bar Animate
+    window.sr = ScrollReveal();
+    sr.reveal(".progress-bar", {
+       origin: "left",
+       duration: 2000,
+       distance: "100%"
+    });
+    
+    
+    // Contact Text Animate
+    const sentences = ["web designer ?", "web developer ?"];
+    let i = 0;
+    
+    setInterval(function() {
+        $(".text-animate").fadeOut(function() {
+            $(this).text(sentences[i = (i + 1) % sentences.length]).fadeIn();
+        });
+    }, 2500);
+    
+});
+
+function downloadDocument() {
+    // Definir la ruta del documento a descargar
+    var urlDocumento = "CVIngles.pdf";
+    
+    // Crear un elemento "a" en el DOM
+    var link = document.createElement("a");
+    
+    // Establecer la ruta del documento en el atributo "href" del elemento "a"
+    link.href = urlDocumento;
+    
+    // Establecer el atributo "download" del elemento "a" para indicar que es una descarga
+    link.download = "CVIngles.pdf";
+    
+    // Simular un clic en el elemento "a" para iniciar la descarga
+    link.click();
+  }
