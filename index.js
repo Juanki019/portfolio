657ef49da33e8ea4c58d3c1e61506cab08152a76
Juanki019 <juanki.sgbh@gmail.com>
2023-04-23 20:44:55 +0200

    v2

--- index.js
+++ index.js
@@ -47,4 +47,24 @@ function downloadDocument() {
     
     // Simular un clic en el elemento "a" para iniciar la descarga
     link.click();
-  }
+}
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContents =>{
+            tabContents.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
