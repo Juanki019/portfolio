50a16e278915114f572ee7cfbcd2dc3ed1c5bb70
Juanki019 <juanki.sgbh@gmail.com>
2023-04-28 12:55:38 +0200

    h

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-$(function() {
-
-    // Scroll click on menu
-    $(".scroll").on("click", function() {   
-        $(this).each(function() {
-            const sectionTop = $(this.hash).offset().top;
-            $("html, body").animate({
-                scrollTop: sectionTop
-            }, 1500);
-        });
-    });
-    
-    
-    // Progress Bar Animate
-    window.sr = ScrollReveal();
-    sr.reveal(".progress-bar", {
-       origin: "left",
-       duration: 2000,
-       distance: "100%"
-    });
-    
-    
-    // Contact Text Animate
-    const sentences = ["web designer ?", "web developer ?"];
-    let i = 0;
-    
-    setInterval(function() {
-        $(".text-animate").fadeOut(function() {
-            $(this).text(sentences[i = (i + 1) % sentences.length]).fadeIn();
-        });
-    }, 2500);
-    
-});
 
 function downloadDocument() {
     // Definir la ruta del documento a descargar
@@ -52,19 +19,92 @@ function downloadDocument() {
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab=>{
+tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContents =>{
-            tabContents.classList.remove('qualification__active')
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
 
-        tabs.forEach(tab=>{
+        tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
     })
 })
 
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+    const items = document.querySelectorAll('.slider-item');
+    const itemCount = items.length;
+    const nextItem = document.querySelector('.next');
+    const previousItem = document.querySelector('.previous');
+    const navItem = document.querySelector('a.toggle-nav');
+    let count = 0;
+    
+    function showNextItem() {
+    items[count].classList.remove('active');
+    if(count < itemCount - 1) {
+        count++;
+    } else {
+        count = 0;
+    }
+    items[count].classList.add('active');
+    console.log(count);
+    }
+    
+    function showPreviousItem() {
+    items[count].classList.remove('active');
+    
+    if(count > 0) {
+        count--;
+    } else {
+        count = itemCount - 1;
+    }
+    
+    items[count].classList.add('active');
+      // Check if working...
+    console.log(count);
+    }
+    
+    function toggleNavigation(){
+        this.nextElementSibling.classList.toggle('active');
+    }
+    
+    function keyPress(e) {
+        e = e || window.event;
+        
+        if (e.keyCode == '37') {
+        showPreviousItem();
+        } else if (e.keyCode == '39') {
+        showNextItem();
+    }
+    }
+    
+    nextItem.addEventListener('click', showNextItem);
+    previousItem.addEventListener('click', showPreviousItem);
+    document.addEventListener('keydown', keyPress);
+    navItem.addEventListener('click', toggleNavigation);});
