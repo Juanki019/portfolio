7bacf44cfd888b6c858755d175245359b415d91e
Juanki019 <juanki.sgbh@gmail.com>
2023-05-02 18:55:16 +0200

    v1

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 
 function downloadDocument() {
-    // Definir la ruta del documento a descargar
     var urlDocumento = "CVIngles.pdf";
     
-    // Crear un elemento "a" en el DOM
     var link = document.createElement("a");
     
-    // Establecer la ruta del documento en el atributo "href" del elemento "a"
     link.href = urlDocumento;
     
-    // Establecer el atributo "download" del elemento "a" para indicar que es una descarga
     link.download = "CVEnglish.pdf";
     
-    // Simular un clic en el elemento "a" para iniciar la descarga
     link.click();
 }
-
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')
-
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
-
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification__active')
-        })
-        target.classList.add('qualification__active')
-
-        tabs.forEach(tab =>{
-            tab.classList.remove('qualification__active')
-        })
-        tab.classList.add('qualification__active')
-    })
-})
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -170,4 +146,13 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-})
+
+const hamburgerMenu = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav__list');
+
+hamburgerMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+
+});
